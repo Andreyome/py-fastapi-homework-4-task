9eb2cd1c7a5c0e437921f90546fe28a4c5cf37a3
Andrii Omelchenko <andreyomem@gmail.com>
2025-09-01 12:50:50 +0300

    fix flake8

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -300,10 +300,8 @@ async def request_password_reset_token(
     status_code=status.HTTP_200_OK,
     responses={
         400: {
-            "description": (
-                    "Bad Request - The provided email or token is invalid, "
-                    "the token has expired, or the user account is not active."
-            ),
+            "description": ("Bad Request - The provided email or token is invalid, "
+                            "the token has expired, or the user account is not active."),
             "content": {
                 "application/json": {
                     "examples": {
